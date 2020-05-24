d4df4a479d4c04087508cfffc2a65af05a84acba
Anish Moorthy <anlsh@protonmail.com>
2020-05-24 00:46:19 -0400

    Update package spec [add-tomaster]

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,6 +1,7 @@
 (defpackage picl-tests
   (:use :cl :picl)
-  (:local-nicknames (:f :fiveam) (:gcl :generic-cl) (:alx :alexandria))
+  (:local-nicknames (#:f #:fiveam) (#:gcl #:generic-cl) (#:alx #:alexandria))
   (:export #:picl-test-suite))
 
+(in-package :picl-tests)
 (f:def-suite picl-test-suite :description "Tests for Python Iterators in CL")
