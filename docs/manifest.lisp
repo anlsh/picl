fa1f12e239c68d6e0c211c1b3d4d0aad4a3543b4
Anish Moorthy <anlsh@protonmail.com>
2020-07-04 17:23:00 -0400

    Ignore the docs dir for now

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:picl)
- :documents ((:title "PICL"
-              :authors ("Anish Moorthy")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("manual.scr"))))
