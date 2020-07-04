999fac125c1743b20d4c9d876a019813a415ac0f
Anish Moorthy <anlsh@protonmail.com>
2020-07-03 23:53:19 -0400

    Set up documentation using Codex

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:picl)
+ :documents ((:title "PICL"
+              :authors ("Anish Moorthy")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
--- picl.asd
+++ picl.asd
@@ -6,7 +6,7 @@
   :license  "MIT"
   :version "0.0.1"
   :serial t
-  :depends-on (#:defclass-std #:alexandria #:iterate)
+  :depends-on (#:defclass-std #:alexandria #:iterate #:codex)
   :components
   ((:module "src"
     :components ((:file "package")
